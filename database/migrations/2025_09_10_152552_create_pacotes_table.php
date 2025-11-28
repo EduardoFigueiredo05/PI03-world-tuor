3361021fbc6d98e14c81d2a759b7f38d9c3f52f8
Eduardo Figueiredo <eduardo.f@GRUPOAD.LOCAL>
2025-11-28 14:34:42 -0300

    Implementar a estrutura atomic

--- database/migrations/2025_09_10_152552_create_pacotes_table.php
+++ database/migrations/2025_09_10_152552_create_pacotes_table.php
@@ -6,26 +6,27 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    
     public function up(): void
     {
+        Schema::create('pacotes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('continente');
+            $table->string('pais');
+            $table->text('descricao');
+            $table->decimal('preco',8,2);
+            $table->date('data_inicio');
+            $table->date('data_fim');
+            $table->foreignId('image_package_id')
+                  ->nullable()
+                  ->constrained('image_packages')
+                  ->onDelete('set null');
+            // --------------------------------
 
-        //Presta atenção seus safados!!
-        
-    Schema::create('pacotes', function (Blueprint $table) {
-        $table->id(); // Cria uma coluna 'id' auto-incremental como chave primária
-        $table->string('nome'); // Cria uma coluna 'nome' do tipo string (VARCHAR)
-        $table->string('continente'); // Cria uma coluna 'continente' do tipo string
-        $table->string('pais'); // Cria uma coluna 'pais' do tipo string
-        $table->text('descricao'); // Cria uma coluna 'descricao' do tipo texto (TEXT), para descrições longas
-        $table->decimal('preco',8,2); // Cria uma coluna 'preco' do tipo decimal, com até 8 dígitos e 2 casas decimais
-        $table->date('data_inicio'); // Cria uma coluna 'data_inicio' do tipo data (DATE)
-        $table->date('data_fim'); // Cria uma coluna 'data_fim' do tipo data (DATE)
-        $table->timestamps(); // Cria as colunas 'created_at' e 'updated_at' para controle de datas de criação e atualização
-    });
-}
+            $table->timestamps();
+        });
+    }
 
-    
     public function down(): void
     {
         Schema::dropIfExists('pacotes');
--- database/migrations/2025_09_10_152624_create_reservas_table.php
+++ database/migrations/2025_09_10_152624_create_reservas_table.php
@@ -9,18 +9,18 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('reservas', function (Blueprint $table) {
-            $table->id();
-            //Chaves estrangeiras para conectar a reserva com o usuario e pacotes //
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->foreignId('pacote_id')->constrained('pacotes')->onDelete('cascade');
-
-            $table->string('status')->default('pendente');
-            $table->timestamps();
-        });
-    }
+   public function up(): void
+{
+    Schema::create('reservas', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        $table->foreignId('pacote_id')->constrained('pacotes')->onDelete('cascade');
+        $table->date('data_reserva'); // Campo novo
+        $table->integer('qtd_pessoas')->default(1); // Campo novo
+        $table->string('status')->default('pendente');
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
